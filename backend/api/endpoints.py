1ff6c5755f8ae39b24d25af21584b284142324cc
Sergi <sergi@premai.io>
2025-07-22 18:05:51 +0200

    Add episodes endpoint

--- backend/api/endpoints.py
+++ backend/api/endpoints.py
@@ -1,8 +1,14 @@
-from fastapi import APIRouter
+from database import get_session
+from fastapi import APIRouter, Depends
+from fastapi_pagination import Page
+from fastapi_pagination.ext.sqlalchemy import paginate
+from models import Episode
+from sqlmodel import Session, select
 
 router = APIRouter()
 
 
-@router.get("/test")
-def test():
-    return {"test": "test"}
+@router.get("/episodes", tags=["episodis"], response_model=Page[Episode])
+def get_episodes(session: Session = Depends(get_session)):
+    query = select(Episode).order_by(Episode.published_at.desc())
+    return paginate(session, query)
--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,18 @@ from api.endpoints import router as api_router
 from fastapi import FastAPI
 from fastapi_pagination import add_pagination
 
-app = FastAPI()
+tags_metadata = [
+    {
+        "name": "episodis",
+        "description": "Episodis API",
+    }
+]
+
+app = FastAPI(
+    docs_url="/api/docs",
+    redoc_url=None,
+    openapi_tags=tags_metadata,
+)
+
 add_pagination(app)
 app.include_router(api_router, prefix="/api")
