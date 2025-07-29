91e40ceffae2c3835bee21153418a86d3408731b
Sergi <sergi@premai.io>
2025-07-29 11:58:48 +0200

    Add pagination

--- frontend/src/lib/components/app-footer.svelte
+++ frontend/src/lib/components/app-footer.svelte
@@ -2,8 +2,10 @@
   import Github from "lib/components/github.svelte";
 </script>
 
-<footer class="px-10 text-sm text-center">
-  <div class="border-t py-4 flex justify-between items-center gap-4">
+<footer class="px-10 text-sm">
+  <div
+    class="border-t py-4 flex justify-between items-center gap-4 2xl:max-w-screen-2xl 2xl:mx-auto"
+  >
     <div class="w-6 h-6 ml-auto">
       <a href="https://github.com/stp11/en-guardia" target="_blank" rel="noopener noreferrer">
         <Github />
