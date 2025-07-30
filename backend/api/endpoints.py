2da7e4cf3fd4e2292e424a4d1e12cf365953485e
Sergi <sergi@premai.io>
2025-07-30 18:50:44 +0200

    Add sorting

--- backend/api/endpoints.py
+++ backend/api/endpoints.py
@@ -9,11 +9,19 @@ router = APIRouter()
 
 
 @router.get("/episodes", tags=["episodis"], response_model=Page[Episode])
-def get_episodes(session: Session = Depends(get_session), search: str = ""):
-    query = select(Episode).order_by(Episode.published_at.desc())
-
+def get_episodes(
+    session: Session = Depends(get_session),
+    search: str = "",
+    order: str = "desc",
+):
+    query = select(Episode)
     if search.strip():
         search_term = f"%{search.strip()}%"
         query = query.filter(Episode.title.like(search_term))
+
+    if order == "desc":
+        query = query.order_by(Episode.published_at.desc())
+    else:
+        query = query.order_by(Episode.published_at.asc())
 
     return paginate(session, query)
