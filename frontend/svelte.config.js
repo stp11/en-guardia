03efa9c7bb8220894445d6d1a3a687e942304dac
Sergi <sergi.tantinya@gmail.com>
2025-12-08 20:01:08 +0100

    Fix

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -16,7 +16,7 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter({
-      fallback: "200.html",
+      fallback: "index.html",
     }),
     prerender: {
       entries: [],
