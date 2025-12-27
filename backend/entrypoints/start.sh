c7a79fe9913ee42412b72f6dcc923610a813f509
Sergi <sergi.tantinya@gmail.com>
2025-12-27 16:46:41 +0100

    Fix

--- backend/entrypoints/start.sh
+++ backend/entrypoints/start.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 set -e
 
+echo "Starting application on port $PORT"
+
 echo "Running database migrations..."
 alembic upgrade head
 echo "Migrations complete."
 
+# Start uvicorn
+echo "Starting uvicorn..."
 exec uvicorn main:app --host 0.0.0.0 --port $PORT
