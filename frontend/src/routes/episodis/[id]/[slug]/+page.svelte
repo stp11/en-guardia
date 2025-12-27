7c56b90c5f22e455a7bc7f00dcc0a960c509b364
Sergi <sergi.tantinya@gmail.com>
2025-12-27 13:26:58 +0100

    Bump tanstack query and refactor

--- frontend/src/routes/episodis/[id]/[slug]/+page.svelte
+++ frontend/src/routes/episodis/[id]/[slug]/+page.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
   import { capitalize, getCategoryStyles } from "lib/utils";
 
-  import type { PageData } from "../$types";
-
-  export let data: PageData;
+  const { data } = $props();
 </script>
 
 {#if data.episode}
