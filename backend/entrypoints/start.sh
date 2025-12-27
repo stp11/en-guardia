3f88ffa66e0b00c4fa56e2c75b09a69dc015b13a
Sergi <sergi.tantinya@gmail.com>
2025-12-27 16:13:07 +0100

    Fix start script

--- backend/entrypoints/start.sh
+++ backend/entrypoints/start.sh
@@ -5,4 +5,4 @@ echo "Running database migrations..."
 alembic upgrade head
 echo "Migrations complete."
 
-exec uvicorn main:app --host 0.0.0.0 --port $PORT --proxy-headers --forwarded-allow-ips='*'
+exec uvicorn main:app --host 0.0.0.0 --port $PORT
